a802fa318c01c87659e03d282fcdb007dbe44d48
WilliamNylund <william.nylund@gmail.com>
2021-12-15 18:19:15 +0200

    Add sass dependency

--- webshop-frontend/src/index.js
+++ webshop-frontend/src/index.js
@@ -1,5 +1,5 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "./index.css";
+//import 'bootstrap/dist/css/bootstrap.min.css';
+import "./index.scss";
 import React from 'react';
 import ReactDOM from "react-dom";
 import App from './views/App'
