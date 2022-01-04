eff190ca1918578a37149963b2b52236c39bd0b7
WilliamNylund <william.nylund@gmail.com>
2022-01-04 21:25:14 +0200

    Add remove all items button to cart canvas

--- webshop-frontend/src/components/CartOffcanvas.js
+++ webshop-frontend/src/components/CartOffcanvas.js
@@ -4,7 +4,7 @@ import { useState, useEffect, useContext } from "react";
 import { UserContext } from "../contexts/UserContext";
 import axios from "axios";
 
-const CartOffcanvas = ({show, handleClose}) => {
+const CartOffcanvas = ({ show, handleClose }) => {
   const user = useContext(UserContext);
   const [cart, setCart] = useState({});
   const [total, setTotal] = useState("0");
@@ -13,7 +13,7 @@ const CartOffcanvas = ({show, handleClose}) => {
     if (cart && Object.keys(cart).length > 0) {
       let priceTotal = 0;
       Object.keys(cart).map((id, i) => {
-        priceTotal += parseFloat(cart[id].price)
+        priceTotal += parseFloat(cart[id].price);
       });
 
       setTotal(priceTotal.toFixed(2));
@@ -33,72 +33,90 @@ const CartOffcanvas = ({show, handleClose}) => {
 
   const removeProduct = (productId) => {
     const storageCart = JSON.parse(localStorage.getItem("cart")) || {};
-    delete storageCart[productId]
+    delete storageCart[productId];
     localStorage.setItem("cart", JSON.stringify(storageCart));
     setCart(storageCart);
   };
 
-  const pay = () => {
+  const emptyCart = () => {
     setCart({});
     localStorage.removeItem("cart");
   }
+
+  const pay = () => {
+    
+    emptyCart();
+  };
 
   useEffect(() => {
     calculateCartTotal();
   }, [cart]);
 
   return (
-    <Offcanvas show={show} onHide={handleClose} placement="end" onEntering={populateCart}>
-        <Offcanvas.Header closeButton>
-          <Offcanvas.Title><h3>Shopping Cart</h3></Offcanvas.Title>
-        </Offcanvas.Header>
-        <Offcanvas.Body>
+    <Offcanvas
+      show={show}
+      onHide={handleClose}
+      placement="end"
+      onEntering={populateCart}
+    >
+      <Offcanvas.Header closeButton>
+        <Offcanvas.Title>
+          <h3>Shopping Cart</h3>
+        </Offcanvas.Title>
+      </Offcanvas.Header>
+      <Offcanvas.Body>
+        <Row>
+          <Col xs={{ span: 3, offset: 9 }}> 
+            <Button variant="link" size="sm" onClick={emptyCart}>
+              Remove all
+            </Button>
+          </Col>
+        </Row>
         {Object.keys(cart).map((id, index) => (
           <>
-              <Row className="shopping-cart-row">
-                <Col xs="6">{cart[id].title}</Col>
-                <Col xs="4">{cart[id].price} € </Col>
-                <Col xs="2">
-                  <BsXLg
-                    className="clickable"
-                    onClick={() => removeProduct(id)}
-                  />
-                </Col>
-              </Row>
-              <Row className="shopping-cart-line">
-                <Col xs="12">{cart[id].warningText}</Col>
-              </Row>
-            </>
-            ))}
-            <Row className="shopping-cart-bottom-line">
-              <Col xs="6"><strong>Total:</strong></Col>
-              <Col xs="4">{total} €</Col>
-              <Col xs="2"></Col>
-            </Row>
-            <Row>
-              <Col xs={{ span: 4, offset: 6 }} className="mt-1">
-                <Button
-                  size="sm"
-                  onClick={pay}
-                >
-                  Pay
-                </Button>
+            <Row className="shopping-cart-row">
+              <Col xs="6">{cart[id].title}</Col>
+              <Col xs="4">{cart[id].price} € </Col>
+              <Col xs="2">
+                <BsXLg
+                  className="clickable"
+                  onClick={() => removeProduct(id)}
+                />
               </Col>
             </Row>
-        </Offcanvas.Body>
-      </Offcanvas>
-  )
+            <Row className="shopping-cart-line">
+              <Col xs="12">{cart[id].warningText}</Col>
+            </Row>
+          </>
+        ))}
+        <Row className="shopping-cart-bottom-line">
+          <Col xs="6">
+            <strong>Total:</strong>
+          </Col>
+          <Col xs="4">{total} €</Col>
+          <Col xs="2"></Col>
+        </Row>
+        <Row>
+          <Col xs={{ span: 4, offset: 6 }} className="mt-1">
+            <Button size="sm" onClick={pay}>
+              Pay
+            </Button>
+          </Col>
+        </Row>
+      </Offcanvas.Body>
+    </Offcanvas>
+  );
 };
 
 export default CartOffcanvas;
 
 const addWarningTexts = async (cart) => {
-  let filterString = 'ids=';
+  let filterString = "ids=";
   Object.keys(cart).map((id, i) => {
     if (i == 0) {
-      filterString += id
+      filterString += id;
     } else {
-      filterString += ","+id
+      filterString += "," + id;
     }
   });
 
@@ -106,45 +124,14 @@ const addWarningTexts = async (cart) => {
   request.data.map((product, index) => {
     if (product.price != cart[product.id].price) {
       console.log("price changed");
-      cart[product.id].warningText = 'The price of this item has changed to ' + product.price
+      cart[product.id].warningText =
+        "The price of this item has changed to " + product.price;
     }
   });
-  return cart
-}
-
-const getProducts = async (filterString) => {
-  let request = axios.get(
-    `http://127.0.0.1:8000/api/items/?${filterString}`
-  );
-  return request;
+  return cart;
 };
 
-/*
-{
-      id: 570,
-      title: "Kiwi",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      price: "2.85",
-      created_at: "19.12.2021 18:49",
-      owner: 121,
-    },
-    {
-      id: 569,
-      title: "Soy sauce",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      price: "2.12",
-      created_at: "19.12.2021 18:49",
-      owner: 121,
-    },
-    {
-      id: 568,
-      title: "Meatballs",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      price: "2.79",
-      created_at: "19.12.2021 18:49",
-      owner: 121,
-    },
-*/
+const getProducts = async (filterString) => {
+  let request = axios.get(`http://127.0.0.1:8000/api/items/?${filterString}`);
+  return request;
+};
