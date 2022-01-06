71515f065c60746a4fb78652e9cd62305bd24df0
WilliamNylund <william.nylund@gmail.com>
2022-01-06 23:31:44 +0200

    Add switch filters to show item categories in my-items

--- webshop-frontend/src/views/MyItems.js
+++ webshop-frontend/src/views/MyItems.js
@@ -1,22 +1,48 @@
-import { Container } from "react-bootstrap";
-import Header from "../components/Header";
-import AddItemModal from "../components/Modals/AddItemModal";
-import OnSale from "../components/my_items/onSale";
-import Sold from "../components/my_items/Sold";
-import Purchased from "../components/my_items/Purchased";
+import { Container, FormCheck, Row } from 'react-bootstrap';
+import Header from '../components/Header';
+import AddItemModal from '../components/Modals/AddItemModal';
+import OnSale from '../components/my_items/onSale';
+import Sold from '../components/my_items/Sold';
+import Purchased from '../components/my_items/Purchased';
+import { useState } from 'react';
 const MyItems = () => {
-  /*
-  see items on sale, sold and purchased
-  filter items
-  
-  */
+  const [showOnSale, setShowOnSale] = useState(true);
+  const [showSold, setShowSold] = useState(true);
+  const [showPurchased, setShowPurchased] = useState(true);
+
+  const onSaleChanged = (e) => setShowOnSale(!showOnSale);
+  const soldChanged = (e) => setShowSold(!showSold);
+  const purchasedChanged = (e) => setShowPurchased(!showPurchased);
+
   return (
     <Container className="container-fluid">
       <Header text="My items" />
       <AddItemModal />
-      <OnSale/>
-      <Sold/>
-      <Purchased/>
+      <hr />
+      <FormCheck
+        inline
+        checked={showOnSale}
+        type="switch"
+        label="Show on sale items"
+        onClick={onSaleChanged}
+      />
+      <FormCheck
+        inline
+        checked={showSold}
+        type="switch"
+        label="Show sold items"
+        onClick={soldChanged}
+      />
+      <FormCheck
+        inline
+        checked={showPurchased}
+        type="switch"
+        label="Show purchased items"
+        onClick={purchasedChanged}
+      />
+      {showOnSale && <OnSale />}
+      {showSold && <Sold />}
+      {showPurchased && <Purchased />}
     </Container>
   );
 };
