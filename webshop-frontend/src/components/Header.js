6faa38fa868542688c6898357d5ca2156a57b311
WilliamNylund <william.nylund@gmail.com>
2022-01-08 18:10:22 +0200

    Remove redundant background from header

--- webshop-frontend/src/components/Header.js
+++ webshop-frontend/src/components/Header.js
@@ -2,7 +2,7 @@ import { Container } from 'react-bootstrap';
 
 const Header = (props) => {
   return (
-    <Container className="header-container" bg="blue">
+    <Container className="header-container">
       <h3>{props.text}</h3>
     </Container>
   );
